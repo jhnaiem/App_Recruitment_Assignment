dea2d68fafd7555c1188cd81d958f033ad77f334
Md. Jahid Hasan Naiem 1620266642 <jahid.naiem@northsouth.edu>
2020-12-12 20:07:27 +0600

    Added Viewmodel

---
+++ app/src/main/java/com/example/app_recruitment_assignment/MainViewmodel.java
@@ -0,0 +1,11 @@
+package com.example.app_recruitment_assignment;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewmodel extends ViewModel {
+
+
+    public void onSubmitClick(){
+
+    }
+}
