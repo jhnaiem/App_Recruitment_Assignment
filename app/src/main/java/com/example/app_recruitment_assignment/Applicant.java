32ca68f42a6051d6ac8a4572ab0729dce3da71e3
Md. Jahid Hasan Naiem 1620266642 <jahid.naiem@northsouth.edu>
2020-12-15 19:00:39 +0600

    Update Applicant.java

--- app/src/main/java/com/example/app_recruitment_assignment/Applicant.java
+++ app/src/main/java/com/example/app_recruitment_assignment/Applicant.java
@@ -1,8 +1,6 @@
 package com.example.app_recruitment_assignment;
 
-import com.google.gson.annotations.SerializedName;
-
-import java.sql.Timestamp;
+import com.google.gson.JsonObject;
 
 public class Applicant {
     private String tsync_id;
@@ -19,14 +17,14 @@ public class Applicant {
     private long expected_salary;
     private String field_buzz_reference;
     private String github_project_url;
-    private String cv_file;
-    @SerializedName("cv_file.tsync_id")
-    private String cv_file_tsync_id;
-    private Timestamp on_spot_update_time;
-    private Timestamp on_spot_creation_time;
+    private JsonObject cv_file;
+//    @SerializedName("cv_file.tsync_id")
+//    private String cv_file_tsync_id;
+    private Long on_spot_update_time;
+    private Long on_spot_creation_time;
 
 
-    public Applicant(String tsync_id, String name, String email, String phone, String full_address, String name_of_university, int graduation_year, float cgpa, int experience_in_months, String currrent_work_place_name, String applying_in, long expected_salary, String field_buzz_reference, String github_project_url, String cv_file, Timestamp on_spot_update_time, Timestamp on_spot_creation_time) {
+    public Applicant(String tsync_id, String name, String email, String phone, String full_address, String name_of_university, int graduation_year, float cgpa, int experience_in_months, String currrent_work_place_name, String applying_in, long expected_salary, String field_buzz_reference, String github_project_url, JsonObject cv_file, Long on_spot_update_time, Long on_spot_creation_time) {
         this.tsync_id = tsync_id;
         this.name = name;
         this.email = email;
@@ -42,6 +40,7 @@ public class Applicant {
         this.field_buzz_reference = field_buzz_reference;
         this.github_project_url = github_project_url;
         this.cv_file = cv_file;
+//        this.cv_file_tsync_id = cv_file_tsync_id;
         this.on_spot_update_time = on_spot_update_time;
         this.on_spot_creation_time = on_spot_creation_time;
     }
